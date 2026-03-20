453f2eccb9273181506b14c98876222b9812a7b6
Dileep <vrdileep1999@gmail.com>
2026-03-20 13:25:56 +0530

    adding changes

--- transcation-service/src/main/java/com/bank/production/transcation_service/config/RestClientConfig.java
+++ transcation-service/src/main/java/com/bank/production/transcation_service/config/RestClientConfig.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -13,5 +14,10 @@ public class RestClientConfig {
     @LoadBalanced
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder().build();
     }
 }
